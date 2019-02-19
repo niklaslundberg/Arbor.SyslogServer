a27f31d89a53a7aab5e187f955b8cf1168520233
Niklas Lundberg <info@nlundberg.se>
2019-02-19 15:54:22 +0100

    Removed docker for main build.

--- build/build.bat
+++ build/build.bat
@@ -4,16 +4,4 @@ SET Arbor.X.Build.PublishRuntimeIdentifier=win10-x64
 
 CALL dotnet arbor-build
 
-IF "%ERRORLEVEL%" NEQ "0" (
-    EXIT /B %ERRORLEVEL%
-)
-
-CD %~dp0
-
-CD ..
-
-CD docker
-
-powershell .\build-container.bat
-
 EXIT /B %ERRORLEVEL%
